8012e29d239ca93454a32ebafd30fb874c4861bd
爸 <chenjinzhi@zldemacbook-pro.local>
2018-05-09 18:33:21 +0800

    爱上

--- 新闻/任务/viewController/抽奖/Mine_choujiang_ViewController.m
+++ 新闻/任务/viewController/抽奖/Mine_choujiang_ViewController.m
@@ -9,13 +9,15 @@
 #import "Mine_choujiang_ViewController.h"
 #import "choujiang_model.h"
 
-@interface Mine_choujiang_ViewController ()<UIWebViewDelegate>
+@interface Mine_choujiang_ViewController ()<WKUIDelegate,WKNavigationDelegate>
+
+@property (nonatomic,strong)UIProgressView* progressView;
 
 @end
 
 @implementation Mine_choujiang_ViewController{
     UIView*             m_navibar_view;
-    UIWebView*          m_webView;
+    WKWebView*          m_webView;
     NSArray*            m_array_url;
     choujiang_model*    m_choujiang_model;
     BOOL                m_choujiang_isDone;
@@ -36,8 +38,10 @@ static int UrlIndex = 0;//顺序使用url
 
 -(void)viewWillDisappear:(BOOL)animated{
     [super viewWillDisappear:animated];
-    NSString* taskId = [Md5Helper Choujiang_taskId:[Login_info share].userInfo_model.user_id AndUrl:m_choujiang_model.url];
-    [self.delegate choujiang_result:m_choujiang_isDone AndTaskId:taskId];
+    if(m_choujiang_model != nil){
+        NSString* taskId = [Md5Helper Choujiang_taskId:[Login_info share].userInfo_model.user_id AndUrl:m_choujiang_model.url];
+        [self.delegate choujiang_result:m_choujiang_isDone AndTaskId:taskId];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
@@ -157,8 +161,21 @@ static int UrlIndex = 0;//顺序使用url
 }
 
 -(void)initWeb{
-    m_webView = [UIWebView new];
-    m_webView.delegate = self;
+    //进度条初始化
+    self.progressView = [[UIProgressView alloc] initWithFrame:CGRectMake(0, 20, [[UIScreen mainScreen] bounds].size.width, 2)];
+    self.progressView.backgroundColor = [UIColor blueColor];
+    //设置进度条的高度，下面这句代码表示进度条的宽度变为原来的1倍，高度变为原来的1.5倍.
+    self.progressView.transform = CGAffineTransformMakeScale(1.0f, 1.5f);
+    [m_navibar_view addSubview:self.progressView];
+    [self.progressView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.and.right.equalTo(m_navibar_view);
+        make.height.mas_offset(kWidth(2));
+        make.bottom.equalTo(m_navibar_view.mas_bottom);
+    }];
+    
+    m_webView = [WKWebView new];
+    m_webView.UIDelegate            = self;
+    m_webView.navigationDelegate    = self;
 //    NSURL* url = [NSURL URLWithString:@"https://cpu.baidu.com/1033/ea61450f?scid=10463"];
 //    NSURLRequest* request = [NSURLRequest requestWithURL:url];
 //    [m_web loadRequest:request];
@@ -168,6 +185,8 @@ static int UrlIndex = 0;//顺序使用url
         make.top.equalTo(m_navibar_view.mas_bottom);
         make.bottom.equalTo(self.view.mas_bottom);
     }];
+    
+    [m_webView addObserver:self forKeyPath:@"estimatedProgress" options:NSKeyValueObservingOptionNew context:nil];
 }
 
 -(void)loadWeb{
@@ -186,20 +205,69 @@ static int UrlIndex = 0;//顺序使用url
 }
 
 #pragma mark - webview协议
--(void)webViewDidFinishLoad:(UIWebView *)webView{
-    NSLog(@"webViewDidFinishLoad");
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context {
+    if ([keyPath isEqualToString:@"estimatedProgress"]) {
+        self.progressView.progress = m_webView.estimatedProgress;
+        if (self.progressView.progress == 1) {
+            /*
+             *添加一个简单的动画，将progressView的Height变为1.4倍，在开始加载网页的代理中会恢复为1.5倍
+             *动画时长0.25s，延时0.3s后开始动画
+             *动画结束后将progressView隐藏
+             */
+            __weak typeof (self)weakSelf = self;
+            [UIView animateWithDuration:0.25f delay:0.3f options:UIViewAnimationOptionCurveEaseOut animations:^{
+                weakSelf.progressView.transform = CGAffineTransformMakeScale(1.0f, 1.4f);
+            } completion:^(BOOL finished) {
+                weakSelf.progressView.hidden = YES;
+                
+            }];
+        }
+    }else{
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+    }
 }
 
--(BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType{
-
-    NSString* url_now = request.URL.absoluteString;
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationResponse:(WKNavigationResponse *)navigationResponse decisionHandler:(void (^)(WKNavigationResponsePolicy))decisionHandler{
+    
+    NSString* url_now = navigationResponse.response.URL.absoluteString;
     NSRange range = [url_now rangeOfString:m_choujiang_model.keyword];
     if (range.location != NSNotFound) {
         NSLog(@"抽奖完成");
         m_choujiang_isDone = YES;
     }
     
-    return YES;
+//    return YES;
+    NSLog(@"%@",navigationResponse.response.URL.absoluteString);
+    //允许跳转
+    decisionHandler(WKNavigationResponsePolicyAllow);
+    //不允许跳转
+    //decisionHandler(WKNavigationResponsePolicyCancel);
+}
+
+//开始加载
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation {
+    NSLog(@"开始加载网页");
+    //开始加载网页时展示出progressView
+    self.progressView.hidden = NO;
+    //开始加载网页的时候将progressView的Height恢复为1.5倍
+    self.progressView.transform = CGAffineTransformMakeScale(1.0f, 1.5f);
+    //防止progressView被网页挡住
+    [m_navibar_view bringSubviewToFront:self.progressView];
+}
+
+//加载完成
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
+    NSLog(@"加载完成");
+    //加载完成后隐藏progressView
+    self.progressView.hidden = YES;
+}
+
+//加载失败
+- (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(WKNavigation *)navigation withError:(NSError *)error {
+    NSLog(@"加载失败");
+    //加载失败同样需要隐藏progressView
+    self.progressView.hidden = YES;
+    [MBProgressHUD showError:@"加载失败!"];
 }
 
 #pragma mark - 按钮方法
